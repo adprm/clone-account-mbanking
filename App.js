f247c10690391f4aac60fcfcb4f5fcd4e869125e
adprm <aditiyaprmn00@gmail.com>
2020-11-04 12:56:09 +0700

    components

--- App.js
+++ App.js
@@ -13,6 +13,9 @@ const HomeScreen = () => {
         <Text style={styles.name}>Aditiya Permana</Text>
       </View>
       <Saldo />
+      <Transfer />
+      <Mutation />
+      <DateMutation />
       <View style={styles.listItem}>
         <Setting />
         <Help />
@@ -26,17 +29,44 @@ const HomeScreen = () => {
 const Saldo = () => {
   return (
     <View style={styles.saldo}>
-      <Ionicons style={styles.icon} size={35} name='ios-card' />
+      <Ionicons style={styles.icon} size={33} name='ios-card' />
       <Text style={styles.descSaldo}>Saldo</Text>
-      <Text style={{marginLeft: 140}}>Rp. 302.976.000,00</Text>
+      <Text style={{marginLeft: 165}}>Rp. 302.976.000</Text>
     </View>
   );
+}
+
+const Transfer = () => {
+  return (
+    <View style={styles.list}>
+      <Ionicons style={styles.icon} size={33} name='ios-cash' />
+      <Text style={styles.descSaldo}>Transfer</Text>
+    </View>
+  )
+}
+
+const Mutation = () => {
+  return (
+    <View style={styles.list}>
+      <Ionicons style={styles.icon} size={33} name='ios-list' />
+      <Text style={styles.descSaldo}>Daftar Mutasi</Text>
+    </View>
+  )
+}
+
+const DateMutation = () => {
+  return (
+    <View style={styles.list}>
+      <Ionicons style={styles.icon} size={33} name='ios-calendar' />
+      <Text style={styles.descSaldo}>Tanggal Mutasi</Text>
+    </View>
+  )
 }
 
 const Setting = () => {
   return (
     <View style={styles.list}>
-      <Ionicons style={styles.icon} size={35} name='ios-cog' />
+      <Ionicons style={styles.icon} size={33} name='ios-cog' />
       <Text style={styles.descSaldo}>Pengaturan</Text>
     </View>
   )
@@ -45,7 +75,7 @@ const Setting = () => {
 const Help = () => {
   return (
     <View style={styles.list}>
-      <Ionicons style={styles.icon} size={35} name='ios-help-circle-outline' />
+      <Ionicons style={styles.icon} size={33} name='ios-help-circle-outline' />
       <Text style={styles.descSaldo}>Bantuan</Text>
     </View>
   )
@@ -54,7 +84,7 @@ const Help = () => {
 const Terms = () => {
   return (
     <View style={styles.list}>
-      <Ionicons style={styles.icon} size={35} name='ios-paper' />
+      <Ionicons style={styles.icon} size={33} name='ios-paper' />
       <Text style={styles.descSaldo}>Syarat & Ketentuan</Text>
     </View>
   )
@@ -63,7 +93,7 @@ const Terms = () => {
 const Logout = () => {
   return (
     <View style={styles.list}>
-      <Ionicons style={styles.icon} size={35} name='ios-log-out' />
+      <Ionicons style={styles.icon} size={33} name='ios-log-out' />
       <Text style={styles.descSaldo}>Keluar</Text>
     </View>
   )
@@ -77,7 +107,7 @@ const App = () => {
       <Stack.Navigator>
         <Stack.Screen name="Account" component={HomeScreen} options={{
           headerStyle: {
-            backgroundColor: '#14aeff'
+            backgroundColor: '#015daa'
           },
           headerTintColor: '#fff'
         }} />
@@ -123,7 +153,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   icon: {
-    color: '#14aeff',
+    color: '#015daa',
     marginLeft: 10,
     marginRight: 14,
   },

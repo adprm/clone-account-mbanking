5b138d558a48cf98a1a6bae4713a3be10d5f5f2f
adprm <aditiyaprmn00@gmail.com>
2020-11-05 10:53:24 +0700

    import transfer page

--- src/pages/HomeScreen/index.js
+++ src/pages/HomeScreen/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import ProfileAccount from '../ProfileAccount';
 import Saldo from '../Saldo';
+import Trasnfer from '../Transfer';
 
 const HomeScreen = () => {
     return (
         <View style={styles.container}>
             <ProfileAccount />
             <Saldo />
+            <Trasnfer />
             {/* <Saldo />
             <Transfer />
             <Mutation />
@@ -27,5 +29,9 @@ export default HomeScreen;
 const styles = StyleSheet.create({
     container: {
         backgroundColor: '#edece8',
-      },
+    },
+    // ListItem
+    listItem: {
+        marginTop: 4,
+    },
 })

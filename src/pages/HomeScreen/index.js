5c4432e09f4a6eeceab1ecd0b6afefa036c937d1
adprm <aditiyaprmn00@gmail.com>
2020-11-05 11:23:10 +0700

    add scollview

--- src/pages/HomeScreen/index.js
+++ src/pages/HomeScreen/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 import ProfileAccount from '../ProfileAccount';
 import Saldo from '../Saldo';
 import Trasnfer from '../Transfer';
@@ -12,19 +12,21 @@ import Logout from '../Logout';
 
 const HomeScreen = () => {
     return (
-        <View style={styles.container}>
-            <ProfileAccount />
-            <Saldo />
-            <Trasnfer />
-            <Mutation />
-            <DateMutation />
-            <View style={styles.listItem}>
-                <Setting />
-                <Help />
-                <Terms />
-                <Logout />
+        <ScrollView>
+            <View style={styles.container}>
+                <ProfileAccount />
+                <Saldo />
+                <Trasnfer />
+                <Mutation />
+                <DateMutation />
+                <View style={styles.listItem}>
+                    <Setting />
+                    <Help />
+                    <Terms />
+                    <Logout />
+                </View>
             </View>
-        </View>
+        </ScrollView>
     )
 }
 
